96ae7184c0561739d9211ead9c0e8b98f97d9a61
abarkhadle999-cpu <a.barkhadle999@gmail.com>
2026-05-04 12:29:20 +0200

    Update simulering antibiotikaresistens.py

--- simulering antibiotikaresistens.py
+++ simulering antibiotikaresistens.py
@@ -80,7 +80,7 @@ def simulate_step(sensitive, resistant):
     sensitive *= (1 - antibiotic_strength*0.3 )
 
     # Resistenta dör lite (kostnad finns)
-    resistant *= (1 - antibiotic_strength*0.2 )
+    resistant *= (1 - antibiotic_strength*0.1 )
 
     # =====================================================
     # 5. SELEKTIONSFÖRDEL FÖR RESISTENTA
